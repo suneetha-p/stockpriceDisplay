8ea7ab1728f53b19f299bffd24f8f6eb11da26b3
suneetha <suni.ponduri@gmail.com>
2020-06-16 08:58:17 +0800

    removed file

--- target/classes/schema.sql
+++
@@ -1,4 +0,0 @@
-drop table if exists STOCK_PRICE CASCADE ;
- drop sequence if exists hibernate_sequence;
-
-Create  table STOCKPRICE(id number(5,2) auto_increment primary key,STOCK_SYMBOL varchar2(100),bidPrice number(5,2),askPrice number(5,2),EVENT_TIME TIMESTAMP);
